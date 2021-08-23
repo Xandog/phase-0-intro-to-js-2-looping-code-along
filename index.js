328c52d26b286ee8db69f624860aa4eba50367cd
Alex Aguirre <aguirre_alex@rocketmail.com>
2021-08-23 01:27:43 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, surprise ) {
+    let messages = [];
+    for(let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`)
+        console.log(messages[i])
+    }
+    return messages;
+}
+function countDown(integer) {
+
+    while(integer >= 0){
+        console.log(integer--)
+    }
+}
+
+countDown(4);
